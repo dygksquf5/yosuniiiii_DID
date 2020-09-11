a086dd414d84f1de7667b954c4dde733fb5f724c
eunhyeeeee <qpfl0963@naver.com>
2020-09-11 17:41:18 +0900

    qr코드 죽여 ,,

--- Issuer_front_page/public/js/qr.js
+++ Issuer_front_page/public/js/qr.js
@@ -2,4 +2,16 @@ let input = document.querySelector('input');
 let button = document.querySelector('button');
 let qrcode = new QRCode(document.querySelector('#qrcode'),{
 
-})
+    text: "hello",
+    width: 250,
+    height: 250,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+
+});
+
+button.addEventListener('click', ()=> {
+    let inputValue = input.value;
+    qrcode.makeCode(inputValue);
+});
