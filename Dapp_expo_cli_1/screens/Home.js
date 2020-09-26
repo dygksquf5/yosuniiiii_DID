73e5d8261469eaeedb40e15aa75dd1a6e081b8b5
shinaeeeee <totodltlsdo5@naver.com>
2020-09-26 16:05:27 +0900

    카드 수정 !!

--- Dapp_expo_cli_1/screens/Home.js
+++ Dapp_expo_cli_1/screens/Home.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect , Component} from 'react';
-import PropTypes from 'prop-types'
+import React, { useState, useEffect, Component } from 'react';
+import PropTypes from 'prop-types';
 import {
-  StyleSheet, Text, View, StatusBar, TouchableOpacity, TouchableHighlight
-} from 'react-native'
-import Button from '../src/components/Button'
-import { colors } from '../src/theme'
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  TouchableHighlight,
+} from 'react-native';
+import Button from '../src/components/Button';
+import { colors } from '../src/theme';
 import { Container, Header, Content, Card, CardItem, Body } from 'native-base';
 import FontIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { NavigationContainer } from '@react-navigation/native';
@@ -12,105 +17,88 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { render } from 'react-dom';
 import { State } from 'react-native-gesture-handler';
 
-
-
-
 export default class Home extends Component {
+  render() {
+    return (
+      <View style={styles.root}>
+        <View style={{ textAlign: 'left', marginTop: 15 }}>
+          <Text style={styles.name}> O O O 님 ! </Text>
+        </View>
 
-  render(){
-    return(
-      
-<View style={styles.root}>
-    <View>
-<Text style={styles.name}> O O O 님 ! </Text>
+        <Container>
+          <Content>
+            <Card style={styles.card}>
+              <View style={{ margin: 8 }}>
+                <TouchableOpacity onPress={this.gotoDetails}>
+                  <Text style={styles.cardUpText}>나의 신분증 !</Text>
 
-<TouchableOpacity onPress={this.gotoDetails}>
-<Container>
-  <Content style={{marginTop: 30,}}>
-    <Card style={styles.card}>
-      <CardItem header>
-        <Text>나의 신분증 !</Text>
-      </CardItem>
-      <CardItem>
-        <Body>
-{/* <View style={{height: 200}}/> */}
-        </Body>
-      </CardItem>
-      <CardItem footer>
-        <Text style={{marginLeft: 280}}>2022.01.10</Text>
-      </CardItem>
-   </Card>
-  </Content>
-</Container>
-</TouchableOpacity>
-</View>
+                  {/* 신분증 사진 넣게된다면 자리 ! */}
+                  <View style={{ height: 150 }} />
 
-<View style={styles.addButton}>
-      <TouchableHighlight 
-      underlayColor='#ff7043' onPress={this.gotoQR}>
-      <FontIcon
-                  name="qrcode-scan"
-                  color='white'
-                  size={35}
-                  
-                />
-  </TouchableHighlight>
+                  <Text style={styles.cardDownText}>2022.01.10</Text>
+                </TouchableOpacity>
+              </View>
+            </Card>
+          </Content>
+        </Container>
 
+        <View style={styles.addButton}>
+          <TouchableHighlight underlayColor='#ff7043' onPress={this.gotoQR}>
+            <FontIcon name='qrcode-scan' color='white' size={35} />
+          </TouchableHighlight>
+        </View>
       </View>
-    {/* <StatusBar barStyle="light-content" />
-    <Text style={styles.title}>Home</Text> */}
-    {/* <Button
-      title="Go to Details"
-      color="white"
-      backgroundColor={colors.lightPurple}
-      onPress={this.gotoDetails}
-      /> */}
-    {/* <Button style={styles.button}
-      title="Go to 큐알큐알"
-      color="white"
-      backgroundColor={colors.lightPurple}
-      onPress={this.gotoQR}
-      /> */}
-  </View>
-    )
+    );
   }
   gotoQR = () => {
-    this.props.navigation.navigate("QRcode")
-  }
+    this.props.navigation.navigate('QRcode');
+  };
   gotoDetails = () => {
     this.props.navigation.navigate('testtest');
   };
-
 }
-
 
 const styles = StyleSheet.create({
   name: {
-    fontWeight:'bold', 
-    marginTop:75, 
-    marginLeft:30
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginLeft: 20,
   },
+
   root: {
     flex: 1,
     flexDirection: 'column',
-    alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'white',
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
+
   button: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
-  card: { 
-    marginLeft:15, 
-    borderColor:'white', 
-    borderRadius:10,
-    shadowColor: '#000000'
+
+  card: {
+    height: 200,
+    marginLeft: 10,
+    marginRight: 10,
+    shadowColor: '#000000',
+    borderRadius: 20,
+    backgroundColor: '#9966CC',
+    marginTop: 50,
   },
+
+  cardUpText: {
+    color: 'white',
+    textAlign: 'left',
+    marginLeft: 15,
+  },
+
+  cardDownText: {
+    color: 'white',
+    textAlign: 'right',
+    marginRight: 15,
+  },
+
   addButton: {
     backgroundColor: '#9966CC',
     borderColor: '#9966CC',
@@ -122,14 +110,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     position: 'absolute',
     bottom: 50,
-    right:20,
-    shadowColor: "#000000",
+    right: 20,
+    shadowColor: '#000000',
     shadowOpacity: 0.8,
     shadowRadius: 2,
     shadowOffset: {
       height: 1,
-      width: 0
-    }
-  }
-})
-
+      width: 0,
+    },
+  },
+});
--- Dapp_expo_cli_1/src/navigator/stack.js
+++ Dapp_expo_cli_1/src/navigator/stack.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import {createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
-import HomeScreen from "../../screens/HomeScreen"
+import React, { Component } from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import HomeScreen from '../../screens/HomeScreen';
 import SecondScreen from '../../screens/SecondScreen';
 import MainScreen from '../../screens/MainScreen';
 import IdScreen from '../../screens/IdScreen';
@@ -9,17 +9,13 @@ import Home from '../../screens/Home';
 import Details from '../../screens/Details';
 import QRcode from '../../screens/QRcode';
 
-
-
-
 // StackNavigator 객체 생성
-export default  stackNav = createStackNavigator({
-    // Home: {screen:HomeScreen, ncavigationOptions:{header:null}},
-    First: {screen: HomeScreen},
-    Second: {screen: SecondScreen},
-    Home: {screen: Home},
-    testtest: {screen: Details},
-    Id: {screen:IdScreen},
-    QRcode: {screen: QRcode, ncavigationOptions:{header:null}},
-  });
-  
+export default stackNav = createStackNavigator({
+  // Home: {screen:HomeScreen, ncavigationOptions:{header:null}},
+  certification: { screen: HomeScreen },
+  Password: { screen: SecondScreen },
+  Home: { screen: Home },
+  testtest: { screen: Details },
+  Id: { screen: IdScreen },
+  QRcode: { screen: QRcode, ncavigationOptions: { header: null } },
+});
