fe158eef7ed1c1bbacaba79774d8317b4e9ff83c
shinaeeeee <68730098+shinaeeeee@users.noreply.github.com>
2020-10-05 16:30:34 +0900

    인증

--- Prover_Dapp/screens/Profile.js
+++ Prover_Dapp/screens/Profile.js
@@ -6,13 +6,14 @@ import { StyleSheet,
   StatusBar,  
   TouchableOpacity,
   TouchableHighlight,
+  Image
 } from 'react-native';
 import Button from '../src/components/Button';
 import { colors } from '../src/theme';
 import { render } from 'react-dom';
 import Axios from 'axios';
 import FontIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-
+import { Container, Header, Content, Card, CardItem, Body } from 'native-base';
 
 
 
@@ -30,22 +31,47 @@ export default class Profile extends Component {
   render() {
     return (
       <View style={styles.root}>
-        <StatusBar barStyle='light-content' />
-        <Text style={styles.title}>여기는 디 테 일2222 !!</Text>
+        <StatusBar barStyle='light-content'/>
+        {/* <Text style={styles.title}>여기는 디 테 일2222 !!</Text>
         <Button
           title='testtesttest'
           color='white'
           backgroundColor={colors.pink}
           onPress={() => getCred()}
-        />
-      <View
+        /> */}
+      <Container>
+          <Content >
+
+              
+        <View style={styles.view1} >
+        
+          <View style={{width: 137, height: 50, backgroundColor: 'powderblue'}}> 
+          <Image style={{width: 80, height: 50,}}
+         source={require("../assets/images/certification.png")}>
+          </Image>
+          </View>
+
+          <View style={styles.view2} >
+          <Text>어디에서 인증되었나 ~</Text>
+          </View> 
+
+          <View style={styles.view3} >
+           <Text>2020.10.5</Text>
+          </View>
+
+        </View>
+       
+         
+          </Content>
+        </Container>
+      {/* <View
         style={{
           width: '100%',
           flexDirection: 'row',
           justifyContent: 'space-around',
           alignItems: 'center',
         }}>
-      </View>
+      </View> */}
 
       {/* <View style={styles.addButton2}>
           <TouchableHighlight
@@ -65,7 +91,7 @@ export default class Profile extends Component {
   };
 }
 
-
+// style={{width: 137, height: 50,  backgroundColor: 'steelblue'}}
 const styles = StyleSheet.create({
   root: {
     flex: 1,
@@ -74,29 +100,47 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'white',
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  addButton2: {
+  view1:{
+    flex: 1,
+    flexDirection: 'row',
+    shadowColor: '#A4A4A4',
+    shadowOpacity: 0.8,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 4,
+      width: 1,
+    },
+    borderRadius: 14,
     backgroundColor: '#FAFAFA',
     borderColor: '#FAFAFA',
-    borderWidth: 1,
-    height: 65,
-    width: 65,
-    borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    bottom: 130,
-    right: 20,
-    shadowColor: '#000000',
+  },
+  view2:{
+    width: 137,
+    height: 50,
+    backgroundColor: 'skyblue',
+    shadowColor: '#A4A4A4',
     shadowOpacity: 0.8,
-    shadowRadius: 2,
+    shadowRadius: 3,
     shadowOffset: {
-      height: 1,
-      width: 0,
+      height: 4,
+      width: 1,
     },
+    borderColor: '#FAFAFA',
+  },
+  view3:{
+    width: 137,
+    height: 50,
+    backgroundColor: 'steelblue',
+    shadowColor: '#A4A4A4',
+    shadowOpacity: 0.8,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 4,
+      width: 1,
+    },
+    borderColor: '#FAFAFA',
+
   },
 
-});
+  
+  });
